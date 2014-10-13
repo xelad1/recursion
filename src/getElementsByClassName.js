2e74475ccb0f1325a28d02286a90ed268d3020d8
Alexander Dajani <xelad1@gmail.com>
2014-10-12 20:07:03 -0700

    Finished getElementsByClassName
    Implemented recursive function using Crockford's "Walk the DOM".

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,36 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
+//Crockford's "Walk the DOM".  Recursively goes through the entire tree of nodes
+function walkTheDOM(node, func) {
+
+	func(node);
+	node = node.firstChild;
+	while(node) {
+		walkTheDOM(node, func);
+		node = node.nextSibling;
+	}
+}
+
+//implemented using Crockford's "Walk the DOM" method.
+var getElementsByClassName = function (className) {
+	var res = [];
+
+	walkTheDOM(document.body, function (node) {
+		var oldNode = node.className;
+
+		if(oldNode) {
+		var newNode = oldNode.split(' ');
+		for(var i = 0; i < newNode.length; i ++) {
+			if(newNode[i] === className) {
+				res.push(node);
+				break;
+			}
+		}
+	}
+	});
+
+	return res;
 };
+
+
